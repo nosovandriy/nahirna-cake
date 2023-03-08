5e57db01a9c39eb6f746f5173172cd410c236941
Andriy Nosov <nosovandriy@gmail.com>
2023-03-08 17:17:03 +0200

    feature/catalog refactoring catalog

--- app/components/catalog-preview/catalog-preview.tsx
+++ app/components/catalog-preview/catalog-preview.tsx
@@ -1,32 +1,13 @@
 "use client";
 
-import Image from "next/image";
-
-import { useCatalog } from "app/data-catalog/data";
+import CatalogData from "@catalog-data/catalog-data";
 
 const CatalogPreview = () => {
-  const { catalog } = useCatalog();
-
+  const isFullCatalog = false;
   return (
-    <section className="flex flex-col items-center gap-10  pb-[120px]">
-      <h2>Каталог</h2>
-      <div className="grid grid-cols-3 gap-10">
-        {catalog.map((cake) => (
-          <div key={cake.id} className="flex flex-col gap-4">
-            <Image
-              src={cake.imageTitle}
-              alt={cake.description}
-              width={408}
-              height={408}
-            />
-            <h2>{cake.title}</h2>
-            <p className="text-2xl font-medium text-themeBrown-100">
-              від 780 грн
-            </p>
-          </div>
-        ))}
-      </div>
-      <button className="secondaryButton">Більше</button>
+    <section className="flex flex-col my-28">
+      <CatalogData isFullCatalog={isFullCatalog} />
+      <button className="secondaryButton mx-auto mt-10">Більше</button>
     </section>
   );
 };
